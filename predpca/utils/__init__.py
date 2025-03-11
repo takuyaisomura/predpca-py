84abd464040576fba8dc606429d99e66ed5962de
yosider <yosider.on.earth@gmail.com>
2025-03-11 19:19:56 +0900

    Fix: Import torch-dependent utils only after checking if torch exists

--- predpca/utils/__init__.py
+++ predpca/utils/__init__.py
@@ -1,6 +1,18 @@
-from .device_manager import DeviceManager
 from .pcacov import pcacov
-from .pcacov_torch import pcacov as pcacov_torch
-from .sym_positive_torch import sym_positive
 
-__all__ = ["DeviceManager", "pcacov", "pcacov_torch", "sym_positive"]
+# Check if torch is installed
+try:
+    import torch  # noqa: F401
+
+    _has_torch = True
+except ImportError:
+    _has_torch = False
+
+if _has_torch:
+    from .device_manager import DeviceManager  # noqa: F401
+    from .pcacov_torch import pcacov as pcacov_torch  # noqa: F401
+    from .sym_positive_torch import sym_positive  # noqa: F401
+
+    __all__ = ["DeviceManager", "pcacov", "pcacov_torch", "sym_positive"]
+else:
+    __all__ = ["pcacov"]
